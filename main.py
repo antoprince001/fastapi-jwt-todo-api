014e68882138b3ccdc98787bd595c9d9ae430d4c
antoprince001 <antoprince001@gmail.com>
2024-04-06 19:59:29 +0530

    Add todo rest endpoints

---
+++ main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI
+from models import Todo
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message" : "Hello World"}
+
+todos = []
+
+
+# Get all todos
+@app.get("/todos")
+async def get_todos():
+    return {"todos": todos}
+
+
+# Get single todo
+@app.get("/todos/{todo_id}")
+async def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return {"Message" : "No todos found"}
+
+
+# Create a todo
+@app.post("/todos")
+async def create_todo(todo: Todo):
+    todos.append(todo)
+    return {"message": "Todo added"}
+
+
+# Update a todo
+@app.put("/todos/{todo_id}")
+async def update_todo(todo_id: int, todo_obj: Todo):
+    for todo in todos:
+        if todo.id == todo_id:
+            todo.id = todo_id
+            todo.item = todo_obj.item
+            return {"todo": todo}
+    return {"Message" : "No todos found"}
+
+
+# Delete a todo
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            todos.remove(todo)
+            return {"message": "Todo has been deleted"}
+    return {"Message" : "No todos found"}
+
+
+# Suppoers http basic oauth2 tokens (JWT tokens) and built in
+# docs, redoc
+# tests
